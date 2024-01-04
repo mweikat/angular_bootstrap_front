c5745c9a4a898e914083b707b9887a22278c56a8
jose luis <mweikat@hotmail.com>
2024-01-03 23:13:17 -0300

    add

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  apiAuth:'tuUrlProduccion/api/v1/auth',
+  recaptcha: {
+    siteKey: 'google-public-key-production',
+  },
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  apiAuth:'http://localhost:8000/api/v1/auth',
+  recaptcha: {
+    siteKey: 'google-public-key-production',
+  },
+}
+
